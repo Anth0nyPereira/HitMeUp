fa3d9ce48bb54d879c84bbe6ecad1ceb16db37fc
Anth0nyPereira <anthonypereira@ua.pt>
2022-01-05 17:00:14 +0000

    reuploaded example of shaders because the other ones were outdated

--- shaders/vertex_shader_Illumination_pertriangle.glsl
+++ shaders/vertex_shader_Illumination_pertriangle.glsl
@@ -1,10 +1,21 @@
+/*
+Visual Computing 2021/2022
+--------------------------
+
+Vertex shader implementing flat shading. It uses the "flat" qualifier to avoid interpolation for the fragment shader.
+This basically means that the only data passed to the fragment shader is the one coming from the provoking vertex.
+Ask what this is...
+
+Samuel Silva, Nov. 2021
+*/
+
 #version 330
 
 layout (location = 0) in vec3 position;
 layout (location = 1) in vec3 color;
 layout (location = 2) in vec3 normal;
 
-out vec4 vColor;
+flat out vec4 vColor;
 out vec3 vNormal;
 
 
@@ -22,16 +33,18 @@ uniform vec4 lightColor;
 
 // these are properties of the OBJECT
 struct Material {
-    vec4 ambient;
-    vec4 diffuse;
-    vec4 specular;
+    vec4 ambient; // Ka
+    vec4 diffuse; // Kd
+    vec4 specular; // Ks
     float shininess;
 };
 
 uniform Material material;
 
+
 void main()
 {
+    // vertex position after transformations
     gl_Position = projection * view * model * vec4(position, 1.0);
 
     vec3 pos = vec3(model * vec4(position, 1.0));
@@ -45,6 +58,8 @@ void main()
 
 
 
+    // diffuse component
+
     vec3 lightDir;
 
     if (lightPosition[3] == 0.0)    // directional light
@@ -52,19 +67,16 @@ void main()
     else
         lightDir = normalize(lightPosition.xyz - pos);
 
-    // diffuse component
-
     float diff = max(dot(vNormal, lightDir), 0.0);
-
-//    vec4 diffuse = diff * material.diffuse * lightColor;
+    vec4 diffuse = diff * material.diffuse * lightColor;
 
     // specular
-//    float specularStrength = 0.5;
-//    vec3 viewDir = normalize(viewerPosition - pos);
-//    vec3 reflectDir = reflect(lightDir, vNormal);
-//    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
-//    vec4 specular = specularStrength * spec * material.specular * lightColor;
+    float specularStrength = 0.5;
+    vec3 viewDir = normalize(viewerPosition - pos);
+    vec3 reflectDir = reflect(lightDir, vNormal);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+    vec4 specular = specularStrength * spec * material.specular * lightColor;
 
-    // something needs to be done, here
-    vColor = vec4(0.0, 0.0, 1.0, 1.0);
+    // the line below should be expanded to include the different light components
+    vColor = ambient + diffuse + specular;
 }
