b42ef301809d63b88f402427762025884a4e8ceb
Anth0nyPereira <anthonypereira@ua.pt>
2022-01-12 15:28:26 +0000

    added code that recreates the shooting, need to chunk it better

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from panda3d.bullet import BulletWorld
 from panda3d.core import loadPrcFile, Point3, Vec2, CollisionTraverser, \
     CollisionHandlerQueue, CollisionNode, BitMask32, CollisionRay, NodePath, \
     Shader, DirectionalLight, ShadeModelAttrib, PointLight, Material, \
-    AmbientLight, CardMaker  # funct import to load configurations file
+    AmbientLight, CardMaker, CollisionSphere, LPoint3f  # funct import to load configurations file
 from direct.showbase.ShowBase import ShowBase
 from panda3d.core import Vec4, Vec3
-from panda3d.physics import ForceNode, LinearVectorForce
+from panda3d.physics import ForceNode, LinearVectorForce, PhysicsCollisionHandler, ActorNode
 
 from axis_helper import AxisHelper
 from apple import Apple
@@ -198,13 +198,7 @@ class Game(ShowBase):
                 else:
                     print("Wrong, try again!")
 
-    def shoot(self):
-        print("shooting")
-        mousePos = self.mouseWatcherNode.getMouse()
-        mousePos3d = (mousePos[0], 0, mousePos[1])
-        print(mousePos3d)
 
-        # mousePosButton = button.getRelativePoint(self.render, mousePos3d)
 
     def add_shader2(self):
         print("entering add_shader2")
@@ -212,13 +206,116 @@ class Game(ShowBase):
         # available_apples[1].node().setAttrib(ShadeModelAttrib.make(ShadeModelAttrib.MSmooth))
 
     def create_card_maker(self):
+        # create the plane
+
         cm = CardMaker("plane")
         # set the size of the card (left, right, bottom, top - in XZ-plane)
-        cm.setFrame(-0.5, 0.5, -0.5, 0.5)
-        self.card_maker = self.render.attachNewNode(cm.generate())
+        cm.setFrame(-2, 2, -2, 2)
+        self.plane = self.render.attachNewNode(cm.generate())
+        # the card is created vertically in the XZ-plane, so it has to be rotated
+        # to make it horizontal
+        # self.plane.setP(-90.)
+        self.plane.setPos(0, -5, 10)
 
         road_tex = self.loader.loadTexture("textures/sample.jpg")
-        self.card_maker.setTexture(road_tex)
+        self.plane.setTexture(road_tex)
+
+        # collision
+        # enable physics
+        self.enableParticles()
+
+        # set the gravity
+        gravityFN = ForceNode('world-forces')
+        gravityFNP = self.render.attachNewNode(gravityFN)
+        gravityForce = LinearVectorForce(0, 0, -9.81)  # gravity acceleration
+        gravityFN.addForce(gravityForce)
+        self.physicsMgr.addLinearForce(gravityForce)
+
+        # enable collision handling
+        self.cTrav = CollisionTraverser('bullet sample traverser')
+        # self.cTrav.showCollisions(self.render) # É ESTA LINHA CARALHOOOOOOO
+        # print(self.cTrav.showCollisions(self.render))
+        # self.cTrav.setRespectPrevTransform(True)
+        self.pusher = PhysicsCollisionHandler()
+        self.pusher.addInPattern('%fn-hit')
+
+        self.accept("space", self.shoot)
+
+        self.bulletIndex = 0
+        self.bullets = []
+
+        self.panda = self.loader.loadModel("models/panda")
+        self.panda.reparentTo(self.render)
+        self.panda.setCollideMask(BitMask32.bit(1))
+
+        self.bulletIndex = 0
+        self.bullets = []
+
+        self.accept("space", self.shoot)
+
+    def shoot(self):
+        self.bullets.append(self.shootBullet())
+
+    def bulletHit(self, bulletAN, lvf, entry):
+        # print(lvf)
+        # print(type(bulletAN))
+        # print(bulletAN.get_children())
+        bulletAN.getPhysical(0).removeLinearForce(lvf)
+
+    def shootBullet(self):
+        # setup the base node for our bullet
+        bulletNP = NodePath("Bullet-%02d" % self.bulletIndex)
+        print(bulletNP.getName())
+        self.bulletIndex += 1
+        bulletNP.setPos(0, self.camera.getY(), 5)
+        bulletNP.reparentTo(self.render)
+        # create a node that will enable physics
+        bulletAN = ActorNode("bullet-physics")
+        # 3.3g
+        bulletAN.getPhysicsObject().setMass(0.033)
+        # attach the base node to the physics node
+        # this will give us the following nodepath structure
+        # render < bulletNP < bulletANP < bulletAN (actual node)
+        bulletANP = bulletNP.attachNewNode(bulletAN)
+        # attach the node also to the physic manager so it will be
+        # affected by the physic simulation
+        self.physicsMgr.attachPhysicalNode(bulletAN)
+        # load a visual model to represent the bullet
+        bullet = self.loader.loadModel("models/smiley")
+        color = LPoint3f(1, 0, 0)
+        bullet.setColor(color.getX(), color.getY(), color.getZ(), 1.0)
+        bullet.reparentTo(bulletANP)
+        tex = self.loader.loadTexture("maps/noise.rgb")
+        bullet.setTexture(tex, 1)  # the second parameter is the priority
+
+        # setup the collision detection
+        bulletSphere = CollisionSphere(0, 0, 0, 1)
+        bulletCollision = bulletANP.attachNewNode(CollisionNode("bulletCollision-%02d" % self.bulletIndex))
+        bulletCollision.node().addSolid(bulletSphere)
+        # bulletCollision.show()
+        self.pusher.addCollider(bulletCollision, bulletANP)
+        self.cTrav.addCollider(bulletCollision, self.pusher)
+
+        bulletFN = ForceNode('Bullet-force')
+        bulletFNP = bulletNP.attachNewNode(bulletFN)
+        # 150 fps
+        lvf = LinearVectorForce(0, 60, 0)
+        lvf.setMassDependent(1)
+        bulletFN.addForce(lvf)
+        bulletAN.getPhysical(0).addLinearForce(lvf)
+
+        self.accept("bulletCollision-%02d-hit" % self.bulletIndex, self.bulletHit, extraArgs=[bulletAN, lvf])
+
+        self.taskMgr.doMethodLater(5, self.doRemove, 'doRemove',
+                                   extraArgs=[bulletNP],
+                                   appendTask=True)
+
+        return bulletNP
+
+    def doRemove(self, bulletNP, task):
+        bulletNP.removeNode()
+        self.bullets.remove(bulletNP)
+        return task.done
 
     # update loop
     def update(self, task):
