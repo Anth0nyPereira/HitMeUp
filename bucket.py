e758b7c11844111159eaf43c24e7dfa95d0e0a8b
Anth0nyPereira <anthonypereira@ua.pt>
2022-01-16 22:15:56 +0000

    applied 3 textures to each bucket

--- bucket.py
+++ bucket.py
@@ -1,9 +1,22 @@
+from panda3d.core import TextureStage
+
+
 class Bucket:
     def __init__(self, loader):
         self.loader = loader
-        self.bucket = self.loader.loadModel("objects/bucket.obj")
+        self.bucket = self.loader.loadModel("objects/OldBucket.obj")
         self.bucket.setScale(0.3, 0.4, 0.3)
         self.bucket.setP(-90)
+
+        # textures experiment
+        self.first_tex = self.loader.loadTexture("objects/Outside_Diffuse.tif")
+        self.second_tex = self.loader.loadTexture("objects/Outside_Glossiness.tif")
+        self.ts1 = TextureStage('ts1')
+        self.third_tex = self.loader.loadTexture("objects/Outside_Specular.tif")
+        self.ts2 = TextureStage('ts2')
+        self.bucket.setTexture(self.first_tex)
+        self.bucket.setTexture(self.ts1, self.second_tex)
+        self.bucket.setTexture(self.ts2, self.third_tex)
 
     def get_bucket(self):
         return self.bucket
